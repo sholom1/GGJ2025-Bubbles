ef0878f391bca44ddb4f93105ab0267e9fe3a419
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2025-01-25 10:48:15 -0500

    Cleanup

--- Assets/Fan.cs
+++ Assets/Fan.cs
@@ -8,18 +8,15 @@ public class Fan : MonoBehaviour
     private float force = 10f;
     private void OnTriggerStay2D(Collider2D other)
     {
-        // If the object that enters the trigger is the player
-        // then apply a force to the player
+        // If an object that stays in the trigger
+        // then apply a force every frame.
         // The force is applied in the direction of the fan
         // Scaled by the players distance from the fan
-        
-            Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
-        Vector2 direction = -transform.right; //(other.transform.position - transform.position).normalized;
-            float distance = Vector2.Distance(other.transform.position, transform.position);
-        float fanRange = captureArea.transform.localScale.x;// * transform.localScale.x;
-            Debug.Log("Distance: " + distance);
-            Debug.Log("Fan Range: " + fanRange);
-            rb.AddForce(direction * ((fanRange - Mathf.Min(fanRange, distance)) / fanRange) * force, ForceMode2D.Force);
+        Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+        Vector2 direction = -transform.right;
+        float distance = Vector2.Distance(other.transform.position, transform.position);
+        float fanRange = captureArea.transform.localScale.x;
+        rb.AddForce(direction * ((fanRange - Mathf.Min(fanRange, distance)) / fanRange) * force, ForceMode2D.Force);
         
     }
 }
