8e0f0b30f7cec41999af6db1f474c87b9851f27d
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2025-01-25 15:45:38 -0500

    Timer (#5)
    * simple timer
    
    * finished timer and effect

---
+++ Assets/Timer.cs
@@ -0,0 +1,45 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    private float duration;
+    [SerializeField]
+    private TextMeshProUGUI timer;
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    private float pulseThreshold;
+    public UnityEvent OnEnd;
+    private float timeRemaining;
+    private void Start()
+    {
+        timeRemaining = duration;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!animator.GetBool("Pulse"))
+            timeRemaining -= Time.deltaTime;
+
+        timer.text = timeRemaining.ToString("0");
+
+        if (timeRemaining <= pulseThreshold)
+        {
+            animator.SetBool("Pulse", true);
+        }
+
+        if (timeRemaining <= 0)
+        {
+            OnEnd.Invoke();
+            enabled = false;
+        }
+    }
+    public void TickTimerAnim(AnimationEvent animationEvent)
+    {
+        timeRemaining = Mathf.Max(0, timeRemaining - 1);
+    }
+}
