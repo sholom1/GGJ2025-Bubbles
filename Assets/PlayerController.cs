2577864a7f487dc27114545bd59cc1d0b0c3bc43
yftachman <yftachman@gmail.com>
2025-01-25 12:47:11 -0500

    basic movement and jump

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,16 +1,64 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    protected float moveSpeed = 5;
+
+    [SerializeField]
+    protected float jumpForce = 5;
+
+    protected Rigidbody2D rb;
+
+
+    [SerializeField]
+    protected Transform groundCheck;
+
+    [SerializeField]
+    protected float groundCheckDistance;
+
+    [SerializeField]
+    protected LayerMask groundLayer;
+
+    
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    public virtual void OnMove(InputValue value)
     {
-        
+        var inputValues = value.Get<Vector2>();
+        if (inputValues == null) {
+            return;
+        }
+        if (!IsGroundDetected()) {
+            // Cant move while in the air
+            inputValues.y = 0;
+        }
+
+        rb.linearVelocity = new Vector2(
+            rb.linearVelocityX + inputValues.x * moveSpeed,
+            rb.linearVelocityY + inputValues.y * jumpForce
+        );
+    }
+
+    // public virtual bool IsGroundDetected()
+    // {
+    //     var groundLayer = ;
+    //     return Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, groundLayer);
+    // }
+
+public virtual bool IsGroundDetected() =>
+        Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, groundLayer);
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawLine(
+            groundCheck.position,
+            new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance)
+        );
     }
 }
---
+++ Assets/UrchinPlayerController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UrchinPlayerController : PlayerController
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public override void OnMove(InputValue value)
+    {
+        base.OnMove(value);
+
+    }
+}
