2006a18992a269bccd59eb2b95ff94d68f262936
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2025-01-25 11:46:30 -0500

    Player type assignment

---
+++ Assets/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<PlayerController> players = new List<PlayerController>();
+    public static GameManager instance;
+    private void Awake()
+    {
+        if (instance)
+        {
+            Destroy(instance.gameObject);
+            Debug.LogError("Duplicate game manager detected!");
+        }
+        instance = this;
+    }
+    public void RegisterPlayer(PlayerController player)
+    {
+        players.Add(player);
+        if (players.Count == 1)
+        {
+            //Player is bubble
+            player.SetPlayerType(PlayerType.Bubble);
+        }
+        else
+        {
+            //Player is urchin
+            player.SetPlayerType(PlayerType.Urchin);
+        }
+    }
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,15 +2,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private Sprite bubble;
+    [SerializeField]
+    private Sprite urchin;
+    [SerializeField]
+    private new SpriteRenderer renderer;
+    private PlayerType _type = PlayerType.Unassigned;
+    private void Start()
     {
-        
+        GameManager.instance.RegisterPlayer(this);
     }
+    public void SetPlayerType(PlayerType value)
+    {
+        if (_type != PlayerType.Unassigned)
+        {
+            Debug.LogError($"Player is already assigned to {_type}");
+            return;
+        }
+        if (value == PlayerType.Bubble)
+        {
+            // Add bubble specific componets;
+            renderer.sprite = bubble;
+        }
+        else
+        {
+            // Add urchin specific components;
+            renderer.sprite = urchin;
+        }
+    }
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public enum PlayerType
+{
+    Unassigned,
+    Bubble,
+    Urchin
 }
